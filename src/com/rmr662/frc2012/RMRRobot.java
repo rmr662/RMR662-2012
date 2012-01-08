7a0d8e4872d072727af450610c9a6344a6272386
Matt Coffin <mcoffin@irys.com>
2012-01-07 17:25:55 -0700

    INITIAL COMMIT
    Add RMRRobot class. Add Component.java and make the needed corrections to the robot class to make the Component API function as expected.

---
+++ src/com/rmr662/frc2012/RMRRobot.java
@@ -0,0 +1,55 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) FIRST 2008. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package com.rmr662.frc2012;
+
+
+import com.rmr662.frc2012.generic.Component;
+import edu.wpi.first.wpilibj.SimpleRobot;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the SimpleRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
+ */
+public class RMRRobot extends SimpleRobot {
+    
+    private Component[] components;
+    
+    /**
+     * This function is called once each time the robot enters autonomous mode.
+     */
+    public void autonomous() {
+        updateComponents();
+    }
+
+    /**
+     * This function is called once each time the robot enters operator control.
+     */
+    public void operatorControl() {
+        updateComponents();
+    }
+    
+    /**
+     * This function is called exactly once when the robot is powered on.
+     */
+    protected void robotInit() {
+        // TODO: Initialize all components by adding them to components array
+    }
+    
+    /**
+     * This method is called to update all of the physical components
+     */
+    private void updateComponents() {
+        for (int i = 0; i < components.length; i++) {
+            components[i].update();
+        }
+    }
+    
+}
---
+++ src/com/rmr662/frc2012/generic/Component.java
@@ -0,0 +1,49 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rmr662.frc2012.generic;
+
+/**
+ * This class represents a physical subsystem of the robot.
+ * @author RMR662
+ */
+public abstract class Component {
+    
+    /**
+     * Represents the enabled or disabled state of the Component
+     */
+    private boolean enabled;
+    
+    /**
+     * Sets the enabled or disabled state of the component
+     * @param enabled whether the component should be enabled or not
+     */
+    public void setEnabled(boolean enabled) {
+        if (!enabled) {
+            disable();
+        }
+        this.enabled = enabled;
+    }
+    
+    /**
+     * Retrieves the enabled or disabled state of the component
+     * @return whether the component is currently enabled or not
+     */
+    public boolean isEnabled() {
+        return enabled;
+    }
+    
+    /**
+     * Updates the physical state of the component based on its current state
+     * and its target state.
+     */
+    public abstract void update();
+    
+    /**
+     * Immediately disables all parts of the component that may be moving when
+     * the component is disabled.
+     */
+    public abstract void disable();
+    
+}
