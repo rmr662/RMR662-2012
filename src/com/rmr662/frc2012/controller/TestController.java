e37412a5cb93c856ec53eedf92a4dfc4674b18ae
Matt Coffin <mcoffin@irys.com>
2012-01-13 16:18:51 -0700

    Add TestController
    Added a test controller that can be used to test components while they are being developed.

---
+++ src/com/rmr662/frc2012/controller/TestController.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rmr662.frc2012.controller;
+
+import com.rmr662.frc2012.generic.Controller;
+
+/**
+ * A controller that can be used for testing components during development.
+ * @author RMR2012
+ */
+public class TestController implements Controller {
+    
+    public void run() {
+        
+    }
+    
+    public String getRMRName() {
+        return "Test Controller";
+    }
+    
+}
