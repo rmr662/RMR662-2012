9e9b232a5fde66695ac27d76224973f0f8e33c17
RMR Programming <RMR Programming@172.16.149.51>
2012-01-14 11:39:57 -0700

    Add RMRAccelerometer
    Add RMRAccelerometer class. Initial commit

---
+++ src/com/rmr662/frc2012/physical/RMRAccelerometer.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rmr662.frc2012.physical;
+
+/**
+ * This class should read data from the accelerometer.
+ * @author RMR662
+ */
+public class RMRAccelerometer {
+    
+    public double getAcceleration() {
+        // TODO: read acceleration from accelerometer.
+        return 0.0;
+    }
+    
+}
