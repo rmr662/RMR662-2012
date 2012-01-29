eee834b1aa4c0d2525dd8f48f82bbaf16e558e58
Matt Coffin <mcoffin@irys.com>
2012-01-28 22:46:03 -0700

    Reverted Drive/RMRJaguar/TestController
    These files were accidentally committed while they still containted
    code inteneded only for debugging.

--- src/com/rmr662/frc2012/controller/TestController.java
+++ src/com/rmr662/frc2012/controller/TestController.java
@@ -7,7 +7,6 @@ package com.rmr662.frc2012.controller;
 import com.rmr662.frc2012.RMRRobot;
 import com.rmr662.frc2012.component.Drive;
 import com.rmr662.frc2012.generic.Controller;
-import com.rmr662.frc2012.physical.RMRAccelerometer;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Timer;
 
@@ -16,31 +15,22 @@ import edu.wpi.first.wpilibj.Timer;
  * @author RMR2012
  */
 public class TestController implements Controller {
-    private static final int ACCEL_SLOT = 1;
     
     private static TestController singletonInstance;
+    
     private static final int[] JOYSTICKS = {1, 2};
     
     private Joystick[] joysticks = new Joystick[2];
-    
-    private RMRAccelerometer accel;
     
     public TestController() {
         for (int i = 0; i < joysticks.length; i++) {
             joysticks[i] = new Joystick(JOYSTICKS[i]);
         }
-        
-        accel = new RMRAccelerometer(ACCEL_SLOT);
     }
     
     public void run() {
         while (true) {
-            Drive.getInstance().setTargetValues(joysticks);
-            
-            
-            //System.out.println("x: " + accel.getXAccel());
-            //System.out.println("y: " + accel.getYAccel());
-            System.out.println("z: " + accel.getZAccel());
+                Drive.getInstance().setTargetValues(joysticks);
             Timer.delay(RMRRobot.PERIOD);
         }
     }
--- src/com/rmr662/frc2012/physical/RMRJaguar.java
+++ src/com/rmr662/frc2012/physical/RMRJaguar.java
@@ -13,7 +13,6 @@ import edu.wpi.first.wpilibj.Jaguar;
 public class RMRJaguar extends Jaguar {
     
     private boolean inverted = false;
-
     
     public RMRJaguar(int channel) {
         super(channel);
